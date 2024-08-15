f30c2e3565b57e8439046507300a286f3b4f17e6
Olha Sashchuk <olha.sashchuk@gmail.com>
2024-08-15 22:32:31 +0200

    implemented task

--- api/src/index.js
+++ api/src/index.js
@@ -30,3 +30,33 @@ app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
 
+
+// /future-meals	Respond with all meals in the future (relative to the when datetime)
+app.get("/future-meals", async (req, res) => {
+  const futureMeals = await knex.raw("SELECT * FROM meal WHERE meal.when > NOW()");
+  if (futureMeals.length > 0) {
+      res.send(futureMeals);
+  } else {
+      res.send([]);
+  }
+});
+
+// /past-meals	Respond with all meals in the past (relative to the when datetime)
+app.get("/past-meals", async (req, res) => {
+  const pastMeals = await knex.raw("SELECT * FROM meal WHERE meal.when < NOW()");
+  if (pastMeals.length > 0) {
+      res.send(pastMeals);
+  } else {
+      res.send([]);
+  }
+});
+
+// /all-meals	Respond with all meals sorted by ID
+app.get("/all-meals", async (req, res) => {
+  const meals = await knex.raw("SELECT * FROM meal ORDER BY id ASC");
+  if (meals.length > 0) {
+      res.send(meals);
+  } else {
+      res.send([]);
+  }
+});
